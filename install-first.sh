1b37d6d28d1b05ba1c24b4905b38c9591b75266e
Theo Mathieu <tmathieu.github@fastmail.com>
2019-07-13 15:13:27 +0200

    feat: add editor config to vscode

--- install-first.sh
+++ install-first.sh
@@ -16,5 +16,6 @@ code --install-extension jpoissonnier.vscode-styled-components
 code --install-extension ms-vscode.Go
 code --install-extension vscode-icons-team.vscode-icons
 code --install-extension whizkydee.material-palenight-theme
+code --install-extension EditorConfig.EditorConfig
 
 cp confs/vscode.json ~/Library/Application\ Support/Code/User/settings.json
