b9904fbbf7574353767a4d0688c3cd68055abb7a
Théo Mathieu <tmathieu.github@fastmail.com>
2019-07-22 19:28:52 +0200

    fix: aws directory

--- get-files.sh
+++ get-files.sh
@@ -8,4 +8,4 @@ read username
 
 scp $username@$ip:/Users/$username/Documents/web_dev.pem ~/Documents/web_dev.pem | true
 scp $username@$ip:/Users/$username/.zhistory ~/.zhistory
-scp -r $username@$ip:/Users/$username/.aws ~/.aw
+scp -r $username@$ip:/Users/$username/.aws ~/.aws
