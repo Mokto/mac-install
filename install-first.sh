a2381135d4459fb241ae4c74b42c4170548b0165
Théo Mathieu <tmathieu.github@fastmail.com>
2019-07-22 13:40:06 +0200

    feat: gitlens

--- install-first.sh
+++ install-first.sh
@@ -19,5 +19,6 @@ code --install-extension ms-vscode.Go
 code --install-extension vscode-icons-team.vscode-icons
 code --install-extension whizkydee.material-palenight-theme
 code --install-extension EditorConfig.EditorConfig
+code --install-extension eamodio.gitlens
 
 cp confs/vscode.json ~/Library/Application\ Support/Code/User/settings.json
