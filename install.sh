f97ad82a8fe5a7c50ff26d238f954d004614f9d7
Mokto <tmathieu.github@fastmail.com>
2020-10-13 08:17:10 +0200

    feat: added jq & pulumi

--- install.sh
+++ install.sh
@@ -36,7 +36,11 @@ brew cask install \
     ngrok \
     whatsapp \
     postman \
-    karabiner-elements
+    karabiner-elements \
+    tunnelblick \
+    openjdk@11
+
+sudo ln -sfn $(brew --prefix)/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk
 
 brew tap homebrew/cask-drivers
 brew cask install logitech-options
@@ -49,7 +53,9 @@ brew install  \
     kubernetes-cli \
     kubernetes-helm \
     go \
-    mercurial
+    mercurial \
+    jq \
+    pulumi
 
 # RUBY + BUNDLER
 # TODO
@@ -79,8 +85,20 @@ npm i -g npm-check-updates
 
 ./utils/mac-settings.sh
 
+# Docker
 defaults write com.apple.dock tilesize -int 50
 defaults write com.apple.dock autohide -bool false
+# key repeat
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 12
+# Hot corners
+defaults write com.apple.dock wvous-tl-corner -int 0
+defaults write com.apple.dock wvous-tr-corner -int 0
+defaults write com.apple.dock wvous-bl-corner -int 0
+defaults write com.apple.dock wvous-br-corner -int 0
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool true
+
 killall Dock
 
 
