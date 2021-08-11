46e7c875dae102e0db92a4795c578b71887d41e4
Théo Mathieu <tmathieu.github@fastmail.com>
2021-08-11 08:26:41 +0200

    Update install-zsh.sh

--- install-zsh.sh
+++ install-zsh.sh
@@ -28,3 +28,12 @@ cp -f ./zsh/.env ~/.env
 compaudit | xargs sudo chmod g-w
 sudo chown -R $(whoami) /usr/local/share/zsh /usr/local/share/zsh/site-functions
 chmod u+w /usr/local/share/zsh /usr/local/share/zsh/site-functions
+
+# install color scripts
+
+git clone https://gitlab.com/dwt1/shell-color-scripts.git
+cd shell-color-scripts
+rm -rf /opt/shell-color-scripts || return 1
+sudo mkdir -p /opt/shell-color-scripts/colorscripts || return 1
+sudo cp -rf colorscripts/* /opt/shell-color-scripts/colorscripts
+sudo cp colorscript.sh /usr/local/bin/colorscript
