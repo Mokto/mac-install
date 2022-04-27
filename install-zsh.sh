7d0f1ed4311a64c8b52dc4172f3353904c4f33c9
Théo Mathieu <tmathieu.github@fastmail.com>
2022-04-27 09:18:17 +0200

    Update install-zsh.sh

--- install-zsh.sh
+++ install-zsh.sh
@@ -3,7 +3,7 @@
 # Install prezto
 rm -rf "${ZDOTDIR:-$HOME}/.zprezto" || true
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-#setopt EXTENDED_GLOB
+setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
     ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
